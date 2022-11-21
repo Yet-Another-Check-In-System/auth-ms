b204828cd5b4d8e4613faa96458c3676195b4532
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-11-21 19:25:10 +0200

    Remove previous Swagger config

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,6 +1,4 @@
 import { Router } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDoc from './v1/swagger_api_v1.json';
 
 import { router as healthRouter } from './v1/healthRouter';
 import { router as userRouter } from './v1/userRouter';
@@ -8,9 +6,6 @@ import { router as permissionRouter } from './v1/permissionRouter';
 import { router as userGroupRouter } from './v1/userGroupRouter';
 
 const router = Router();
-
-router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDoc));
 
 router.use('/health', healthRouter);
 router.use('/auth', userRouter);
